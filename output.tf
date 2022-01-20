bfe38ce226cef568176cce5bcc59e796effb4217
Ayush Rastogi <31894500+ayush14rastogi@users.noreply.github.com>
2022-01-20 19:43:01 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,25 +1,9 @@
-/*
-#output "ayush-webserser-id" {
-#        value = "${module.myapp-webserver.ec2_instance_id}"
-#
-#}
+output "alb_dns_name" {
+        value = "${module.myapp-alb.alb_dns}"
 
-#output "ayush-subnet" {
-#        value = "${module.myapp-subnet.subnet_id}"
-#
-#}
+}
 
 
-#output "ayush-subnet_ips" {
-#        value = "${module.myapp-subnet.subnet_ips}"
-#
-#}
-
-#output "myvpc_ip" {
-#	value = "${module.myapp-subnet.vpc_id}"
-#}
-*/
-## to fetch ec2 public ip
 output "webserver_ips" {
         value = "${module.myapp-webserver.ec2_public_ip}"
 
